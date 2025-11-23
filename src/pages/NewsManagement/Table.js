2f4d5d4613246193cbcfde290e86319059aace27
Parsa <mparsabahredar@gmail.com>
2025-11-23 20:47:21 +0330

    C

--- src/pages/NewsManagement/Table.js
+++ src/pages/NewsManagement/Table.js
@@ -10,185 +10,28 @@ import ReactPaginate from "react-paginate";
 import DataTable from "react-data-table-component";
 import { ChevronDown, Share, Printer, FileText, File, Grid, Copy, } from "react-feather";
 import { selectThemeColors } from "@utils";
-import {
-  Row,
-  Col,
-  Card,
-  Input,
-  Label,
-  Button,
-  CardBody,
-  CardTitle,
-  CardHeader,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle,
-  UncontrolledDropdown,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "reactstrap";
-
+import { Row, Col, Card, Input, Label, Button, CardBody, CardTitle, CardHeader, DropdownMenu, DropdownItem, DropdownToggle,
+UncontrolledDropdown, Modal, ModalHeader, ModalBody, ModalFooter,} from "reactstrap";
 import "@styles/react/libs/react-select/_react-select.scss";
 import "@styles/react/libs/tables/react-dataTable-component.scss";
+import CustomHeader from './CustomHeader/CustomHeader'
 
-const CustomHeader = ({
-  store,
-  toggleSidebar,
-  handlePerPage,
-  rowsPerPage,
-  handleFilter,
-  searchTerm,
-}) => {
-  // ** Converts table to CSV
-  function convertArrayOfObjectsToCSV(array) {
-    let result;
-
-    const columnDelimiter = ",";
-    const lineDelimiter = "\n";
-    const keys = Object.keys(store.data[0]);
-
-    result = "";
-    result += keys.join(columnDelimiter);
-    result += lineDelimiter;
-
-    array.forEach((item) => {
-      let ctr = 0;
-      keys.forEach((key) => {
-        if (ctr > 0) result += columnDelimiter;
-
-        result += item[key];
-
-        ctr++;
-      });
-      result += lineDelimiter;
-    });
-
-    return result;
-  }
-
-  // ** Downloads CSV
-  function downloadCSV(array) {
-    const link = document.createElement("a");
-    let csv = convertArrayOfObjectsToCSV(array);
-    if (csv === null) return;
-
-    const filename = "export.csv";
-
-    if (!csv.match(/^data:text\/csv/i)) {
-      csv = `data:text/csv;charset=utf-8,${csv}`;
-    }
-
-    link.setAttribute("href", encodeURI(csv));
-    link.setAttribute("download", filename);
-    link.click();
-  }
-  return (
-    <div className="invoice-list-table-header w-100 me-1 ms-50 mt-2 mb-75">
-      <Row>
-        <Col xl="6" className="d-flex align-items-center p-0">
-          <div className="d-flex align-items-center w-100">
-            <label style={{ fontSize: "17px" }} htmlFor="rows-per-page">
-              تعداد نمایش :
-            </label>
-            <Input
-              className="mx-50"
-              type="select"
-              id="rows-per-page"
-              value={rowsPerPage}
-              onChange={handlePerPage}
-              style={{ width: "5rem" }}
-            >
-              <option value="10">10</option>
-              <option value="25">25</option>
-              <option value="50">50</option>
-            </Input>
-          </div>
-        </Col>
-        <Col
-          xl="6"
-          className="d-flex align-items-sm-center justify-content-xl-end justify-content-start flex-xl-nowrap flex-wrap flex-sm-row flex-column pe-xl-1 p-0 mt-xl-0 mt-1"
-        >
-          <button onClick={onSearch()} className="d-flex align-items-center mb-sm-0 mb-1 me-1  ">
-            <label
-              style={{ fontSize: "17px" }}
-              className="mb-0 text-nowrap"
-              htmlFor="search-invoice"
-            >
-              جستجو :
-            </label>
-            <Input
-              id="search-invoice"
-              className="ms-50 w-100"
-              type="text"
-              value={searchTerm}
-              onChange={(e) => handleFilter(e.target.value)}
-              placeholder="نام کاربر..."
-            />
-          </button>
-
-          <div className="d-flex align-items-center table-header-actions">
-            <UncontrolledDropdown className="me-1">
-              <DropdownToggle color="secondary" caret outline>
-                <Share className="font-small-4 me-50" />
-                <span className="align-middle">خروجی</span>
-              </DropdownToggle>
-              <DropdownMenu>
-                <DropdownItem className="w-100">
-                  <Printer className="font-small-4 me-50" />
-                  <span className="align-middle">Print</span>
-                </DropdownItem>
-                <DropdownItem
-                  className="w-100"
-                  onClick={() => downloadCSV(store.data)}
-                >
-                  <FileText className="font-small-4 me-50" />
-                  <span className="align-middle">CSV</span>
-                </DropdownItem>
-                <DropdownItem className="w-100">
-                  <Grid className="font-small-4 me-50" />
-                  <span className="align-middle">Excel</span>
-                </DropdownItem>
-                <DropdownItem className="w-100">
-                  <File className="font-small-4 me-50" />
-                  <span className="align-middle">PDF</span>
-                </DropdownItem>
-                <DropdownItem className="w-100">
-                  <Copy className="font-small-4 me-50" />
-                  <span className="align-middle">Copy</span>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            <Button
-              className="add-new-user"
-              color="primary"
-              onClick={toggleSidebar}
-            >
-              اضافه کردن کاربر
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
 
 const NewsList = () => {
 
-
-
   const [searchQuery, setSearchQuery] = useState()
-  const onSearch = (searchTerm) => {
+  const handleSearch = (searchTerm) => {
     setSearchQuery(searchTerm)
   }
 
 
   const { data: newsData, isLoading } = useQuery({
-    queryKey: ["GETNEWS"],
+    queryKey: ["GETNEWS", searchQuery],
     queryFn: () => getNews({Query: searchQuery}),
   });
+
+
+
 
 
 
@@ -404,24 +247,6 @@ const NewsList = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
   return (
     <Fragment>
       {/* <Card>
@@ -530,6 +355,7 @@ const NewsList = () => {
             data={newsData?.news || []}
             subHeaderComponent={
               <CustomHeader
+                handleSearch={handleSearch}
                 store={store}
                 searchTerm={searchTerm}
                 rowsPerPage={rowsPerPage}

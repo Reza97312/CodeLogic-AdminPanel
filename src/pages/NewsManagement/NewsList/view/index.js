646186588802407153eead2467aa1a67ff9a3fb1
Parsa <mparsabahredar@gmail.com>
2025-11-27 17:44:25 +0330

    C

--- src/pages/NewsManagement/NewsList/view/index.js
+++ src/pages/NewsManagement/NewsList/view/index.js
@@ -1,62 +1,63 @@
 // ** React Imports
-import { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
-
-// ** Store & Actions
-import { getUser } from '../store'
-import { useSelector, useDispatch } from 'react-redux'
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
 // ** Reactstrap Imports
-import { Row, Col, Alert } from 'reactstrap'
+import { Row, Col, Alert } from "reactstrap";
 
 // ** User View Components
-import UserTabs from './Tabs'
-import PlanCard from './PlanCard'
-import UserInfoCard from './UserInfoCard'
-
+import UserTabs from "../../../../components/Courses/CourseView/view/courseTabs";
+import PlanCard from "../../../UserManagement/view/PlanCard";
+import UserInfoCard from "../../../UserManagement/view/UserInfoCard";
+import { useQuery } from "@tanstack/react-query";
 // ** Styles
-import '@styles/react/apps/app-users.scss'
+import "@styles/react/apps/app-users.scss";
+import getNews from "../../../../core/services/api/get/getNews";
+import NewsCardInfo from "../../../../components/news/NewsCardInfo/NewsCardInfo";
+import loading from "../../../../assets/images/A/loading.gif";
+import CourseTabs from "../../../../components/Courses/CourseView/view/courseTabs";
 
-const UserView = () => {
-  // ** Store Vars
-  const store = useSelector(state => state.users)
-  const dispatch = useDispatch()
+import store from '../store'
 
+const NewsView = () => {
   // ** Hooks
-  const { id } = useParams()
+  const { id } = useParams();
+  const { data: newsData = {}, isPending } = useQuery({
+    queryKey: ["GETNEWS"],
+    queryFn: () => getNews(id),
+  });
+  const [active, setActive] = useState("1");
 
-  // ** Get suer on mount
-  useEffect(() => {
-    dispatch(getUser(parseInt(id)))
-  }, [dispatch])
-
-  const [active, setActive] = useState('1')
-
-  const toggleTab = tab => {
+  const toggleTab = (tab) => {
     if (active !== tab) {
-      setActive(tab)
+      setActive(tab);
     }
-  }
+  };
 
-  return store.selectedUser !== null && store.selectedUser !== undefined ? (
-    <div className='app-user-view'>
+  
+  return newsData ? (
+    <div className="app-user-view">
       <Row>
-        <Col xl='4' lg='5' xs={{ order: 1 }} md={{ order: 0, size: 5 }}>
-          <UserInfoCard selectedUser={store.selectedUser} />
-          <PlanCard />
+        <Col xl="4" lg="5" xs={{ order: 1 }} md={{ order: 0, size: 5 }}>
+          {isPending ? (
+            <img className="mx-auto" src={loading} />
+          ) : (
+            <NewsCardInfo selectedNews={newsData} />
+          )}
         </Col>
-        <Col xl='8' lg='7' xs={{ order: 0 }} md={{ order: 1, size: 7 }}>
-          <UserTabs active={active} toggleTab={toggleTab} />
+        <Col xl="8" lg="7" xs={{ order: 0 }} md={{ order: 1, size: 7 }}>
+          <CourseTabs id={id} active={active} toggleTab={toggleTab} />
         </Col>
       </Row>
     </div>
   ) : (
-    <Alert color='danger'>
-      <h4 className='alert-heading'>User not found</h4>
-      <div className='alert-body'>
-        User with id: {id} doesn't exist. Check list of all Users: <Link to='/apps/user/list'>Users List</Link>
+    <Alert color="danger">
+      <h4 className="alert-heading">User not found</h4>
+      <div className="alert-body">
+        User with id: {id} doesn't exist. Check list of all Users:{" "}
+        <Link to="/apps/user/list">Users List</Link>
       </div>
     </Alert>
-  )
-}
-export default UserView
+  );
+};
+export default NewsView;

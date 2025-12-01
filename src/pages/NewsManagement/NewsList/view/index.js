99979fff213e0c13d39843c979c2ec0bddee5d53
Parsa <mparsabahredar@gmail.com>
2025-12-01 04:26:59 +0330

    C

--- src/pages/NewsManagement/NewsList/view/index.js
+++ src/pages/NewsManagement/NewsList/view/index.js
@@ -1,29 +1,18 @@
-// ** React Imports
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import getAdminNewsComments from '../../../../core/services/api/get/getAdminNewsComments'
-
-// ** Reactstrap Imports
 import { Row, Col, Alert } from "reactstrap";
-
-// ** User View Components
-// import UserTabs from "../../../../components/Courses/CourseView/view/courseTabs";
-import PlanCard from "../../../UserManagement/view/PlanCard";
-import UserInfoCard from "../../../UserManagement/view/UserInfoCard";
 import { useQuery } from "@tanstack/react-query";
-
-// ** Styles
 import "@styles/react/apps/app-users.scss";
 import getNews from "../../../../core/services/api/get/getNews";
 import NewsCardInfo from "../../../../components/news/NewsCardInfo/NewsCardInfo";
 import loading from "../../../../assets/images/A/loading.gif";
 import Tabs from "./Tabs";
-
 import store from "../store";
 
 
 const NewsView = () => {
-  // ** Hooks
+  
   const { id } = useParams();
   const { data: newsData = {}, isPending } = useQuery({
     queryKey: ["GETNEWS"],
